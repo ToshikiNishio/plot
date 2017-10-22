2d25fe6043c5ba688de94a03eeb7f4d6513f64ea
nishio <toshiki@nishiotoshiterunoMac-Pro.local>
2017-10-22 16:32:20 +0900

    それぞれの処理の関数化

--- plot.py
+++ plot.py
@@ -12,32 +12,32 @@ import pandas as pd
 import glob
 import re
 
-csv_files = glob.glob('*/RUN*/output.csv')
-# 平均適応度を計算する
-mean = pd.read_csv(csv_files[0], header=0, sep=',')
-mean = mean - mean
-for f in csv_files:
-    mean += pd.read_csv(f, header=0, sep=',')
 
-mean /= len(csv_files)
-mean = mean.rename(columns={'Best_fitness': 'Mean Fitness'})
-mean.to_csv('mean.csv')
-print("output mean.csv")
-print(mean)
+def getMean():
+    csv_files = glob.glob('*/RUN*/output.csv')
+    # 平均適応度を計算する
+    mean = pd.read_csv(csv_files[0], header=0, sep=',')
+    mean = mean - mean
+    for f in csv_files:
+        mean += pd.read_csv(f, header=0, sep=',')
 
-print('平均適応度と比較する試行を入力してください。　(Exam: 4)')
-print('全ての試行と比較する場合 : all')
-print('平均適応度だけ出力する場合 : mean')
-num = input('>>>  ')
-if num.isdigit():
-    num_int = int(num)
+    mean /= len(csv_files)
+    mean = mean.rename(columns={'Best_fitness': 'Mean Fitness'})
+    mean.to_csv('mean.csv')
+    # print("output mean.csv")
+    # print(mean)
 
-if num == 'mean':
+    return mean, csv_files
+
+
+def outputMeanFit():
     print('平均適応度を出力します')
     # 平均適応度のプロット
     fit = mean[['eval_times', 'Mean Fitness']]
     fit.plot(x='eval_times', logy=True)     # logスケールでのfitness出力
-elif num == 'all':
+
+
+def outputAllFit():
     print('全ての試行と比較します。')
     # 平均適応度のデータフレーム作成
     fit = mean[['eval_times', 'Mean Fitness']]
@@ -50,12 +50,34 @@ elif num == 'all':
         fit = pd.concat([fit, one_out[rename]], axis=1)
 
     fit.plot(x='eval_times', logy=True)     # logスケールでのfitness出力
-elif 0 <= num_int < len(csv_files):
+
+
+def outputCompFit():
     print('試行' + num + 'と平均適応度の比較を出力します')
+    fit = mean[['eval_times', 'Mean Fitness']]
     file = glob.glob('*/RUN' + num + '/output.csv')
     one_out = pd.read_csv(file[0], header=0, sep=',')
     com_fit = pd.concat([fit, one_out['Best_fitness']], axis=1)
     com_fit = com_fit.rename(columns={'Best_fitness': 'Run' + num})
     com_fit.plot(x='eval_times', logy=True)     # logスケールでのfitness出力else:
-else:
-    print('存在しない試行です')
+
+
+if __name__ == '__main__':
+    print('平均適応度と比較する試行を入力してください。　(Exam: 4)')
+    print('全ての試行と比較する場合 : all')
+    print('平均適応度だけ出力する場合 : mean')
+
+    mean, csv_files = getMean()
+
+    num = input('>>>  ')
+    if num.isdigit():
+        num_int = int(num)
+
+    if num == 'mean':
+        outputMeanFit()
+    elif num == 'all':
+        outputAllFit()
+    elif 0 <= num_int < len(csv_files):
+        outputCompFit()
+    else:
+        print('存在しない試行です')
