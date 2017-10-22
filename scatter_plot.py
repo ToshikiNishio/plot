b7990aafc22099ebedcbfc71bb0dc1ad0e056ff1
nishio <toshiki@nishiotoshiterunoMac-Pro.local>
2017-10-22 17:20:22 +0900

    main関数の作成とinputNum関数の作成

--- scatter_plot.py
+++ scatter_plot.py
@@ -14,13 +14,12 @@ import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 
-swrm_size  # Global definition
 
-
-def _update_plot(i, fig, scat_x, scat_pbest, scat_G):
-    global swrm_size
+def _update_plot(i, fig, scat_x, scat_pbest, scat_G, swrm_size):
+    '''
     print('swrm_size')
     print(swrm_size)
+    '''
     min_num = i * swrm_size
     max_num = min_num + swrm_size
 
@@ -29,17 +28,20 @@ def _update_plot(i, fig, scat_x, scat_pbest, scat_G):
     pbest_sets = np.vstack((horizon_pbest[min_num:max_num],
                             vertical_pbest[min_num:max_num])).T
 
+    '''
     print('x_sets')
     print(x_sets)
     print('pbest_sets')
     print(pbest_sets)
+    '''
 
     scat_x.set_offsets(x_sets)
     scat_pbest.set_offsets(pbest_sets)
     scat_G.set_offsets((horizon_G[i], vertical_G[i]))
 
-    # print(min_num)
-    # print(max_num)
+    '''
+    print(min_num)
+    print(max_num)
 
     print('G')
     print(horizon_G[i])
@@ -50,70 +52,76 @@ def _update_plot(i, fig, scat_x, scat_pbest, scat_G):
     print(vertical_pbest[min_num:max_num])
 
     print('Frames: %d' % i)
+    '''
     plt.title('Generation=' + str(i) + ", Fuction Evaluatio=" + str(min_num))
 
     return scat_x,
 
 
-csv_files = glob.glob('*/RUN*/scatter.csv')
-print('散布図のアニメーションを表示する試行を入力してください。　(Exam: 4)')
-num = input('>>>  ')
-if num.isdigit():
-    num_int = int(num)
+def inputNum():
+    while True:
+        num = input('>>>  ')
+        if num.isdigit():
+            num_int = int(num)
+            return num_int
+        else:
+            print('数値を入力してください')
 
-if 0 <= num_int < len(csv_files):
-    print('試行' + num + 'の散布図のアニメーションを表示します')
-    file = glob.glob('*/RUN' + num + '/scatter.csv')
-    scatter = pd.read_csv(csv_files[0], header=0, sep=',')
+if __name__ == '__main__':
+    csv_files = glob.glob('*/RUN*/scatter.csv')
+    print('散布図のアニメーションを表示する試行を入力してください。　(Exam: 4)')
+    num = inputNum()
 
-    eval_times = scatter['eval_times']
-    eval_times = eval_times.drop_duplicates()
+    if 0 <= num < len(csv_files):
+        print('試行' + str(num) + 'の散布図のアニメーションを表示します')
+        scatter = pd.read_csv(csv_files[num], header=0, sep=',')
 
-    print("x軸,y軸の次元を選択してください")
-    print("x軸の次元 (Exam : 4)")
-    x_dim = input('>>>  ')
-    print("y軸の次元 (Exam : 4)")
-    y_dim = input('>>>  ')
+        # ↓なぜ定義したのか
+        # eval_times = scatter['eval_times']
+        # eval_times = eval_times.drop_duplicates()
 
-    horizon_x = scatter['X' + x_dim]
-    vertical_x = scatter['X' + y_dim]
+        print("x軸,y軸の次元を選択してください")
+        print("x軸の次元 (Exam : 4)")
+        x_dim = input('>>>  ')
+        print("y軸の次元 (Exam : 4)")
+        y_dim = input('>>>  ')
 
-    horizon_pbest = scatter['pBest' + x_dim]
-    vertical_pbest = scatter['pBest' + y_dim]
+        horizon_x = scatter['X' + x_dim]
+        vertical_x = scatter['X' + y_dim]
 
-    horizon_G = scatter[scatter['G_index'] == scatter['individual_index']]
-    horizon_G = horizon_G['pBest' + x_dim]
-    horizon_G.reset_index(drop=True, inplace=True)
-    # horizon_G = horizon_G.copy()
-    vertical_G = scatter[scatter['G_index'] == scatter['individual_index']]
-    vertical_G = vertical_G['pBest' + y_dim]
-    vertical_G.reset_index(drop=True, inplace=True)
+        horizon_pbest = scatter['pBest' + x_dim]
+        vertical_pbest = scatter['pBest' + y_dim]
 
-    # sys.exit()
+        horizon_G = scatter[scatter['G_index'] == scatter['individual_index']]
+        horizon_G = horizon_G['pBest' + x_dim]
+        horizon_G.reset_index(drop=True, inplace=True)
+        # horizon_G = horizon_G.copy()
+        vertical_G = scatter[scatter['G_index'] == scatter['individual_index']]
+        vertical_G = vertical_G['pBest' + y_dim]
+        vertical_G.reset_index(drop=True, inplace=True)
 
-    fig = plt.figure()
+        fig = plt.figure()
 
-    ax = fig.add_subplot(111)
-    ax.grid(True, linestyle='-', color='0.75')
-    ax.set_xlabel('Dim' + x_dim)
-    ax.set_ylabel('Dim' + y_dim)
+        ax = fig.add_subplot(111)
+        ax.grid(True, linestyle='-', color='0.75')
+        ax.set_xlabel('Dim' + x_dim)
+        ax.set_ylabel('Dim' + y_dim)
 
-    scat_x = plt.scatter(horizon_x, vertical_x,
-                         c='red', marker='.', s=20)
-    scat_pbest = plt.scatter(horizon_pbest, vertical_pbest,
-                             c='blue', marker='x', s=20)
-    scat_G = plt.scatter(horizon_G, vertical_G,
-                         c='green', marker='*', s=30)
+        scat_x = plt.scatter(horizon_x, vertical_x,
+                             c='red', marker='.', s=20)
+        scat_pbest = plt.scatter(horizon_pbest, vertical_pbest,
+                                 c='blue', marker='x', s=20)
+        scat_G = plt.scatter(horizon_G, vertical_G,
+                             c='green', marker='*', s=30)
 
-    max_gen = max(scatter['Generation'])
-    swrm_size = max(scatter['individual_index']) + 1
-    print(swrm_size)
-    anim = animation.FuncAnimation(fig, _update_plot,
-                                   fargs=(fig, scat_x, scat_pbest, scat_G),
-                                   frames=max_gen, interval=1)
+        max_gen = max(scatter['Generation'])
+        swrm_size = max(scatter['individual_index']) + 1
+        print(swrm_size)
+        anim = animation.FuncAnimation(fig, _update_plot,
+                                       fargs=(fig, scat_x, scat_pbest, scat_G, swrm_size),
+                                       frames=max_gen, interval=1)
 
-    plt.show()
+        plt.show()
 
-
-else:
-    print('存在しない試行です')
+    else:
+        print('存在しない試行です')
